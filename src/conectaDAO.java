ac1ebdc826816ac1bb0604dad9b3b590d187301c
Renato Rodrigues <renatorodrigues0302@gmail.com>
2024-03-02 22:15:20 -0300

    Localização do Banco de Dados 'settada'

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,8 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?useSSL=false", "root", "78790101SONto");
+            System.out.println("Conectado!");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
